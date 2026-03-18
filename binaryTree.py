1ff61ba42aaf0290478427ccff9dbd2be1bbaaab
Adam Holub <115136431+AdamHolub@users.noreply.github.com>
2026-03-18 23:04:34 +0100

    basic algorithms

---
+++ binaryTree.py
@@ -0,0 +1,95 @@
+from collections import deque
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BinaryTree:
+    @staticmethod
+    def preorder(node):
+        if node is None:
+            return
+        print(node.value, end=' ')
+        BinaryTree.preorder(node.left)
+        BinaryTree.preorder(node.right)
+        
+    @staticmethod
+    def inorder(node):
+        if node is None:
+            return
+        BinaryTree.inorder(node.left)
+        print(node.value, end=' ')
+        BinaryTree.inorder(node.right)
+        
+    @staticmethod
+    def postorder(node):
+        if node is None:
+            return
+        BinaryTree.postorder(node.left)
+        BinaryTree.postorder(node.right)
+        print(node.value, end=' ')
+    
+    @staticmethod
+    def bfs(root):
+        if root is None:
+            return
+        queue = deque([root])
+        while queue:
+            current = queue.popleft()
+            print(current.value, end=' ')
+            if current.left:
+                queue.append(current.left)
+            if current.right:
+                queue.append(current.right)
+    
+    @staticmethod
+    def dfs(root):
+        if root is None:
+            return
+        stack = [root]
+        while stack:
+            current = stack.pop()
+            print(current.value, end=' ')
+            if current.right:
+                stack.append(current.right)
+            if current.left:
+                stack.append(current.left)
+    
+    @staticmethod
+    def height(node):
+        if node is None:
+            return 0
+        left_height = BinaryTree.height(node.left)
+        right_height = BinaryTree.height(node.right)
+        return max(left_height, right_height) + 1
+    
+
+# Binary Tree Example
+root = Node(5)
+root.left = Node(3)
+root.right = Node(8)
+
+root.left.left = Node(1)
+root.left.right = Node(4)
+
+root.right.right = Node(9)
+
+
+BinaryTree.preorder(root)
+print()  # for new line
+
+BinaryTree.inorder(root)
+print()  # for new line
+
+BinaryTree.postorder(root)
+print()  # for new line
+
+BinaryTree.bfs(root)
+print()  # for new line
+
+BinaryTree.dfs(root)
+print()  # for new line
+
+print(BinaryTree.height(root))
